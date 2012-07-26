f7c5202b6883f888a869bca311cbc753004b79d1
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2012-07-26 21:56:20 +0200

    Parsing verbessert

--- src/hcpihc.hs
+++ src/hcpihc.hs
@@ -8,16 +8,30 @@ import System.IO.Error
 
 port = 7331
 
-getHostName = do
-  putStr "Server: "
-  getLine
+getLineWithPromptIfEmpty :: String -> Bool -> String -> IO String
+getLineWithPromptIfEmpty prompt nl string = if string == ""
+  then getLineWithPrompt prompt nl
+  else return string
+
+getLineWithPrompt :: String -> Bool -> IO String
+getLineWithPrompt prompt nl = do
+  putStr prompt
+  line <- getLine
+  if nl
+    then putStrLn ""
+    else return ()
+  return line
+
+safetail :: String -> String
+safetail (x:xs) = xs
+safetail xs = xs
 
 main = withSocketsDo $ do
    hSetBuffering stdout NoBuffering
    args <- getArgs 
    server <- if length args > 0
      then return $ args !! 0
-     else getHostName
+     else getLineWithPrompt "Server: " False
    tryHandle <- tryIOError $ connectTo server $ PortNumber port
    case tryHandle of
      Left a -> putStrLn "Error on connect"
@@ -25,31 +39,29 @@ main = withSocketsDo $ do
        putStrLn "Welcome to the Hauer Chat Protocol In Haskell Client"
        putStrLn "/login <username> to login with username"
        putStrLn "/register <username> to register with the Server"
-       forkIO $ printChats handle
        command <- getLine
        case break (==' ') command of
-         ("/login",x:username) -> do
-           putStr "Password: "
-           password <- withEcho False getLine
-           send handle $ Login username password
+         ("/login",username) -> do
+           username_ <- getLineWithPromptIfEmpty "Username: " False $ safetail username
+           password <- withEcho False $ getLineWithPrompt "Password: " True
+           send handle $ Login username_ password
+           forkIO $ printChats handle
 	   chat handle
-         ("/register",x:username) -> do
-	   putStr "Password: "
-	   password1 <- withEcho False getLine
-	   putStrLn " "
-	   putStr "Repeat password: "
-	   password2 <- withEcho False getLine
-	   putStrLn " "
+         ("/register",username) -> do
+           username_ <- getLineWithPromptIfEmpty "Username: " False $ safetail username
+	   password1 <- withEcho False $ getLineWithPrompt "Password: " True
+	   password2 <- withEcho False $ getLineWithPrompt "Repeat Password: " True
 	   case password1 == password2 of
 	     True -> do
-	       send handle $ Register username password1
+	       send handle $ Register username_ password1
+               forkIO $ printChats handle
 	       chat handle
 	     False -> putStrLn "Passwords don't match"
 	 _ -> putStrLn "Command not recognized"
 
 printChats h = do
+  hFlush stdout
   communication <- tryIOError $ getCommunication h
-  putStrLn $ show communication
   case communication of
     Left a -> putStrLn "Disconnected"
     Right (Error error) -> do
@@ -71,22 +83,28 @@ printChats h = do
       putStrLn $ "* " ++ msg
       printChats h
     _ -> do
-      putStrLn "Seltsam"
       printChats h
-
 
 chat :: Handle -> IO ()
 chat handle = do
-  repeat $ do
-    line <- getLine
-    case break (==' ') line of
-      ("/err",x:errormsg) -> send handle $ Error errormsg
-      ("/dm",x:usernamemessage) -> let (username, message) = break (==' ') usernamemessage in send handle $ DM username message
-      ("/me",x:emote) -> send handle $ Emote emote
-      ("/logout",x:reason) -> send handle $ Logout reason
-      _ -> send handle $ Message line
-  where
-    repeat f = f >>= \_ -> repeat f
+  line <- getLine
+  case break (==' ') line of
+    ("/err",x:errormsg) -> do
+      send handle $ Error $ safetail errormsg 
+      chat handle
+    ("/dm",x:usernamemessage) -> do
+      let (username, message) = break (==' ') usernamemessage
+      send handle $ DM username $ safetail message
+      chat handle
+    ("/me",emote) -> do
+      send handle $ Emote $ safetail emote
+      chat handle
+    ("/logout",reason) -> do
+      send handle $ Logout $ safetail reason
+      return ()
+    _ -> do 
+      send handle $ Message line
+      chat handle
 
 withEcho :: Bool -> IO a -> IO a
 withEcho echo action = do
