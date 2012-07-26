144e5e67dd797bee1eca87b30f3ec8f7f50cf89e
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2012-07-26 21:56:01 +0200

    Bug behoben

--- src/Network/HCPIH/Tainted.hs
+++ src/Network/HCPIH/Tainted.hs
@@ -6,6 +6,7 @@ Communication (Message, Error, Register, Login, Logout, DM, Emote, Undefined)
 
 import System.IO
 import Control.Concurrent
+import Unsafe.Coerce
 
 data Communication = Message String | Error String | Register String String | Login String String | Logout String | DM String String | Emote String | Undefined deriving Show
 
@@ -20,7 +21,7 @@ sendStr (Emote s) = "\6" ++ s ++ "\0"
 
 send :: Handle -> Communication -> IO ThreadId
 send h m = forkIO $ do
-  hPutStrLn h $ sendStr m
+  hPutStr h $ sendStr m
   hFlush h
 
 getCommunication :: Handle -> IO Communication
@@ -30,17 +31,29 @@ getCommunication h = do
     '\0':message -> return $ Error message
     '\1':message -> return $ Message message
     '\2':username -> do
-      password <- hGetToNull h
+      password <- hGetToNull_ h
       return $ Login username password
     '\3':reason -> return $ Logout reason
     '\4':username -> do
-      password <- hGetToNull h
+      password <- hGetToNull_ h
       return $ Register username password
     '\5':username -> do
-      message <- hGetToNull h
+      message <- hGetToNull_ h
       return $ DM username message
     '\6':emote -> return $ Emote emote
     _ -> return Undefined
+
+hGetToNull_ :: Handle -> IO String
+hGetToNull_ h = do
+--  char <- hGetCihar h
+  string <- repeatedGet ""
+  return $ string
+  where 
+    repeatedGet xs = do
+      char <- hGetChar h
+      case char of
+        '\0' -> return xs
+        a -> repeatedGet $ xs++[a]
 
 hGetToNull :: Handle -> IO String
 hGetToNull h = do
